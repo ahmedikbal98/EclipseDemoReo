b1f48065fa2ae02f2346643d20ed991820405f14
2101452 <2101452@LTIN432856.cts.com>
2023-11-16 13:08:35 +0530

    creating a demo controller

---
+++ demoSpring/src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+	
+	@GetMapping("/say")
+	public ResponseEntity<String> sayHello() {
+		
+		return new ResponseEntity<String>("Hello Ikbal Ahmed", HttpStatus.OK);
+	}
+
+}
